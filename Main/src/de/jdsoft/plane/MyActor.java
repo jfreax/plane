3774012b3ebaf5c4e05d35c2145b4dc92632f6e5
Jens Dieskau <jfreax@googlemail.com>
2013-08-16 00:05:31 +0200

    Use ShapeRenderer instead of Textures to draw underground tile "actor"

--- Main/src/de/jdsoft/plane/MyActor.java
+++ Main/src/de/jdsoft/plane/MyActor.java
@@ -10,25 +10,27 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class MyActor extends Actor {
 
-    private final TextureRegion region;
-    private ShapeRenderer renderer;
+    private final ShapeRenderer renderer;
 
     public MyActor () {
-        Pixmap pix = new Pixmap(16, 16, Pixmap.Format.RGB888);
-        pix.setColor(Color.BLUE);
-        pix.fill();
-        Texture tex = new Texture(pix);
-
-        region = new TextureRegion(tex);
-
         renderer = new ShapeRenderer();
     }
 
     public void draw (SpriteBatch batch, float parentAlpha) {
-        Color color = getColor();
-        batch.setColor(color.r, color.g, color.b, color.a * parentAlpha);
-        batch.draw(region, getX(), getY(), getOriginX(), getOriginY(),
-                getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
+        batch.end();
+
+        renderer.setProjectionMatrix(batch.getProjectionMatrix());
+        renderer.setTransformMatrix(batch.getTransformMatrix());
+        renderer.translate(getX(), getY(), 0);
+        renderer.scale(1, 0.5f, 1);
+        renderer.rotate(0, 0, 1, 45.0f);
+
+        renderer.begin(ShapeRenderer.ShapeType.Filled);
+        renderer.setColor(Color.BLUE);
+        renderer.rect(0, 0, 100, 100);
+        renderer.end();
+
+        batch.begin();
 
     }
 }
