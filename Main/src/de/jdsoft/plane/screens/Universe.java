913f295c4d4eecf65b5ccedaa4e96039e8cfb658
Jens Dieskau <jens@dieskau.pm>
2013-08-16 15:56:09 +0200

    Remember camera position after window resize.

--- Main/src/de/jdsoft/plane/screens/Universe.java
+++ Main/src/de/jdsoft/plane/screens/Universe.java
@@ -4,10 +4,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Scaling;
 import de.jdsoft.plane.input.GlobalInput;
 import de.jdsoft.plane.input.OrthoCamController;
+import de.jdsoft.plane.model.Path;
 import de.jdsoft.plane.sprite.Ground;
 
 public class Universe implements Screen {
@@ -22,11 +24,13 @@ public class Universe implements Screen {
 
         stage = new Stage();
 
-        Ground ground1 = new Ground(stage, 0, 0);
-        Ground ground2 = new Ground(stage, 1, 1);
-        Ground ground3 = new Ground(stage, 1, 0);
-        Ground ground4 = new Ground(stage, 2, 0);
-        Ground ground5 = new Ground(stage, 3, 0);
+        new Path(stage, 0, 0);
+        new Path(stage, 1, 0);
+        new Path(stage, 2, 0);
+        new Path(stage, 0, 1);
+
+        //stage.getCamera().position.add(1000);
+        //stage.getCamera().update();
 
         // Stage input processor
         globalInput.addProcessor(stage);
@@ -39,6 +43,7 @@ public class Universe implements Screen {
     @Override
     public void render(float v) {
         Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
 
@@ -46,6 +51,8 @@ public class Universe implements Screen {
     }
     @Override
     public void resize(int width, int height) {
+        Vector3 oldPosition = stage.getCamera().position.cpy();
+
         Vector2 size = Scaling.fit.apply(480, 800, width, height);
         int viewportX = (int)(width - size.x) / 2;
         int viewportY = (int)(height - size.y) / 2;
@@ -53,6 +60,9 @@ public class Universe implements Screen {
         int viewportHeight = (int)size.y;
         Gdx.gl.glViewport(viewportX, viewportY, viewportWidth, viewportHeight);
         stage.setViewport(800, 480, true, viewportX, viewportY, viewportWidth, viewportHeight);
+
+        // Reset camera to old position
+        stage.getCamera().position.set(oldPosition);
     }
 
     @Override
