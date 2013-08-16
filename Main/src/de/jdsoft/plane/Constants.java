31b0c52fe5c93598773375cc78a2bb118e225574
Jens Dieskau <jfreax@googlemail.com>
2013-08-16 08:34:46 +0200

    Constants

---
+++ Main/src/de/jdsoft/plane/Constants.java
@@ -0,0 +1,12 @@
+package de.jdsoft.plane;
+
+
+import com.badlogic.gdx.graphics.Color;
+
+public interface Constants {
+    static final int tileSizeWidth = 64;
+    static final int tileSizeHeight = 64;
+    static final int tileGap = 3;
+
+    static final Color colorPath = new Color(0.286f, 0.4f, 0.58f, 0.f);
+}
